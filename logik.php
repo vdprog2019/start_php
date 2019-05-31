562ea75f66d0877309136656f9307e5ff08934ef
vdprog2019 <48090405+vdprog2019@users.noreply.github.com>
2019-05-31 18:40:42 +0300

    форма

--- logik.php
+++ logik.php
@@ -15,34 +15,65 @@ echo  $var + $one;
 
 
 
-$p = $_POST["plus"];
-switch ($p) {
-    case '+':
-        echo $_POST["first"] + $_POST["second"];
-        break;
-    case '-';
-        echo $_POST["first"] - $_POST["second"];
-        break;
-    case '*':
-        echo $_POST["first"] * $_POST["second"];
-        break;
-    case '/':
-        echo $_POST["first"] / $_POST["second"];
-        break;
-    case '>':
-        if ($_POST["first"] > $_POST["second"]){
-            echo "первое больше";
-        }if ($_POST["second"] > $_POST["first"]){
-            echo "второе больше";
+
+
+echo $_POST["name_user"];
+echo " ";
+echo $_POST["surname"];
+echo " ";
+echo  $_POST["date"];
+
+
+
+
+
+
+if( isset( $_POST['my_file_upload'] ) ){  //isset - проверяет что объект не пустой, готовая функция
+    // ВАЖНО! тут должны быть все проверки безопасности передавемых файлов и вывести ошибки если нужно
+    //использует или создаст папку
+    $uploaddir = './uploads'; // . - текущая папка где находится submit.php
+
+    // cоздадим папку если её нет
+    if( ! is_dir( $uploaddir ) ) mkdir( $uploaddir, 0777 );// mrdir создает папку, 0777 - на сервере
+    $files      = $_FILES; // полученные файлы, $_FILES - содержит массив всех файлов, которые загружал пользователь
+    $done_files = array();
+    // переместим файлы из временной директории в указанную
+    foreach( $files as $file ){
+        $file_name = cyrillic_translit( $file['name'] ); // переводит в кирилицу(англ)
+        if( move_uploaded_file( $file['tmp_name'], "$uploaddir/$file_name" ) ){ // если есть файлы то копировать в папку upload
+            $done_files[] = realpath( "$uploaddir/$file_name" );
         }
-        break;
-    case '<':
-        if ($_POST["first"] < $_POST["second"]){
-            echo "первое меньше";
-        }if ($_POST["second"] < $_POST["first"]){
-            echo "второе меньше";
-        }
-        break;
+    }
+
+    $data = $done_files ? array('files' => $done_files ) : array('error' => 'Ошибка загрузки файлов.');
+
+    die( json_encode( $data ) );
 }
-
-
+## Транслитирация кирилических символов
+function cyrillic_translit( $title ){
+    $iso9_table = array(
+        'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Ѓ' => 'G',
+        'Ґ' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'YO', 'Є' => 'YE',
+        'Ж' => 'ZH', 'З' => 'Z', 'Ѕ' => 'Z', 'И' => 'I', 'Й' => 'J',
+        'Ј' => 'J', 'І' => 'I', 'Ї' => 'YI', 'К' => 'K', 'Ќ' => 'K',
+        'Л' => 'L', 'Љ' => 'L', 'М' => 'M', 'Н' => 'N', 'Њ' => 'N',
+        'О' => 'O', 'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T',
+        'У' => 'U', 'Ў' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'TS',
+        'Ч' => 'CH', 'Џ' => 'DH', 'Ш' => 'SH', 'Щ' => 'SHH', 'Ъ' => '',
+        'Ы' => 'Y', 'Ь' => '', 'Э' => 'E', 'Ю' => 'YU', 'Я' => 'YA',
+        'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'ѓ' => 'g',
+        'ґ' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'є' => 'ye',
+        'ж' => 'zh', 'з' => 'z', 'ѕ' => 'z', 'и' => 'i', 'й' => 'j',
+        'ј' => 'j', 'і' => 'i', 'ї' => 'yi', 'к' => 'k', 'ќ' => 'k',
+        'л' => 'l', 'љ' => 'l', 'м' => 'm', 'н' => 'n', 'њ' => 'n',
+        'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't',
+        'у' => 'u', 'ў' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'ts',
+        'ч' => 'ch', 'џ' => 'dh', 'ш' => 'sh', 'щ' => 'shh', 'ъ' => '',
+        'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya'
+    );
+    $name = strtr( $title, $iso9_table ); // конкантенирует строки
+    $name = preg_replace('~[^A-Za-z0-9\'_\-\.]~', '-', $name );
+    $name = preg_replace('~\-+~', '-', $name ); // --- на -
+    $name = preg_replace('~^-+|-+$~', '', $name ); // кил - на концах
+    return $name;
+}
